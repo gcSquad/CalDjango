b0c2996a919d2ed2956c3f1f22fa1c0093b89afd
Gaurav Chaturvedi <gaurav.pg007@gmail.com>
2019-07-24 14:07:59 +0530

    APIview for login class

--- calendarApi/capi/views.py
+++ calendarApi/capi/views.py
@@ -29,15 +29,6 @@ class Get_assignment_List(APIView):
         serialized = assignedDataSerializer(assignment, many=True)
         return Response(serialized.data)
 
-
-
-def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class Login(APIView):
     template_name = 'login.html'
 
