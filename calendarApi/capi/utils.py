eb176cbae529272a986d8c803c334bfc41326c13
Gaurav Chaturvedi <gaurav.pg007@gmail.com>
2019-08-01 19:33:20 +0530

    timezone handling for assignmnet

--- calendarApi/capi/utils.py
+++ calendarApi/capi/utils.py
@@ -1,9 +1,5 @@
 import datetime
 from django.utils import timezone
-import dateutil.parser
-from dateutil import tz
-import pytz
-
 
 def return_dates_in_isoformat(days_delta,operator):
       
@@ -14,10 +10,4 @@ def return_dates_in_isoformat(days_delta,operator):
 
         date_in_required_format = required_datetime.replace(microsecond=0).isoformat()
         return date_in_required_format 
-
-def convert_into_local_timezone(utc_time,timezone):
-    to_zone = tz.gettz(timezone)
-    from_zone = tz.gettz('UTC')
-    utc_datetime_object=dateutil.parser.parse(utc_time)
-    return (utc_datetime_object.replace(tzinfo=from_zone).astimezone(to_zone)).isoformat()
 
